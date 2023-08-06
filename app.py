3834d23f85a022abeafb5589bc80f305b4662da9
Fadhlan Nur R <fadhlannur007@gmail.com>
2023-08-06 13:10:11 +0700

    Nyoba Github Project

---
+++ app.py
@@ -0,0 +1,9 @@
+import math
+
+def luas_persegi():
+    panjang = int(input("Masukkan Jari-Jari Lingkaran : "))
+    
+    luas = panjang * 2
+    print("Luas Segitiga Adalah : ", luas)
+
+luas_persegi()
