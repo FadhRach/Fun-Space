4a537ed161e6521bbf79b7ed4f726541596f9839
FadhRach <129260006+FadhRach@users.noreply.github.com>
2024-01-15 16:09:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import math
 
 def luas_persegi():
+
+//menginput jari2 lingkaran
     panjang = int(input("Masukkan Jari-Jari Lingkaran : "))
-    
+
+//menghitung luas lingkaran
     luas = panjang * 2
     print("Luas Segitiga Adalah : ", luas)
 
